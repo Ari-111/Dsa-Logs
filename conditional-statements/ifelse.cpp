386bdc36a034132ccc8d9cb505173740e7276c9b
Ari-111 <ashishkmr8800@gamil.com>
2025-02-13 11:44:52 +0530

    if else contional statments

---
+++ conditional-statements/ifelse.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+  /*char ch;
+  cin>>ch;
+  if(ch>='A'&&ch<='Z'){
+    cout<<"uppercase";
+
+  }
+  else if (ch>='a'&&ch<='z'){
+    cout<<"no";
+  }
+  else if (ch>='0'&&ch<='9') {
+    cout<<"digit";
+  }
+
+    return 0;*/
+
+  // char ch;
+  // cin>>ch;
+  // if (isdigit(ch)) {
+  //   cout<< "it is a digit"<<endl;
+  //
+  // }
+  // else if (isupper(ch)) {
+  //
+  //   cout<< "it is a uppercase"<<endl;
+  //
+  // }
+  // else if (islower(ch)) {
+  //   cout<< "it is a lowercase"<<endl;
+  }
+  }
