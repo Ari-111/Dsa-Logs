3535e3bba3fa375552fd7a968e9cb8860a6713ff
Ari-111 <ashishkmr8800@gamil.com>
2025-02-14 17:49:16 +0530

    another pattern

--- pattern2.cpp
+++ pattern2.cpp
@@ -2,16 +2,21 @@
 using namespace std;
 
 
-int main(){
+int main() {
   int n;
   cin>>n;
 
   int  i = 1;
   while(i<=n){
 
-    int j = i;
+    int j = 1;
     while(j<=n){
-      cout<<i<<" "<<j<<endl;
+
+      cout<<i;
+      j = j+1;
     }
+    cout<<endl;
+    i = i+1;
 
   }
+}
